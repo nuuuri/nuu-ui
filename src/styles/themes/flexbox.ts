bf4343e7ae9c0a472ed209e0d87e5ccc386d35d0
nuuuri <sbfl125@gmail.com>
2024-12-04 21:51:44 +0900

    [fix] flexbox props type 수정

--- src/styles/themes/flexbox.ts
+++ src/styles/themes/flexbox.ts
@@ -1,21 +1,14 @@
+import { CSSProperties } from 'react';
+
 import { css } from 'styled-components';
 
-type DirectionType = 'row' | 'column';
-type AlignType = 'left' | 'center' | 'right';
-
-const Align: { [key in AlignType]: string } = {
-  left: 'flex-start',
-  center: 'center',
-  right: 'flex-end',
-};
-
 export const flexbox = (
-  direction: DirectionType = 'row',
-  justify: AlignType = 'center',
-  align: AlignType = 'center'
+  direction: CSSProperties['flexDirection'],
+  justify: CSSProperties['justifyContent'],
+  align: CSSProperties['alignItems']
 ) => css`
   display: flex;
   flex-direction: ${direction};
-  justify-content: ${Align[justify]};
-  align-items: ${Align[align]};
+  justify-content: ${justify};
+  align-items: ${align};
 `;
